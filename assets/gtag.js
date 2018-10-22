65d639cf163f619c46a97e8af9fe2c436bec7608
mihalw28 <michal.waszak@yahoo.com>
2018-10-22 17:04:15 +0200

    u

--- assets/gtag.js
+++ assets/gtag.js
@@ -1,4 +1,4 @@
-<script type="text/javascript"></script>
+
 window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 ga('create', 'UA-127839517-1', 'auto');
 ga('send', 'pageview');
