60bb34c32f9cf414cdaf84fef9277cd7332417da
mihalw28 <michal.waszak@yahoo.com>
2018-10-22 16:09:30 +0200

    u

--- assets/gtag.js
+++ assets/gtag.js
@@ -1,8 +1,8 @@
-src="https://www.googletagmanager.com/gtag/js?id=UA-127839517-1";
 
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  
-  gtag('js', new Date());
-  gtag('config', 'UA-127839517-1');
+  window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+  ga('create', 'UA-127839517-1', 'auto');
+  ga('send', 'pageview');
 
+  async src='https://www.google-analytics.com/analytics.js'
+
+ 
