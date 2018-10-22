a321592be840951de2c226b1ec8e3fa3ffcdd0a7
mihalw28 <michal.waszak@yahoo.com>
2018-10-22 15:53:33 +0200

    u

--- assets/gtag.js
+++ assets/gtag.js
@@ -1,8 +1,8 @@
-async src="https://www.googletagmanager.com/gtag/js?id=UA-127839517-1"
+src="https://www.googletagmanager.com/gtag/js?id=UA-127839517-1";
 
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
+  
   gtag('js', new Date());
-
   gtag('config', 'UA-127839517-1');
 
