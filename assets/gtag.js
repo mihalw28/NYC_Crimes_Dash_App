cfaf899f3e2a611a645c6c6efe7145ccc8b48f2b
mihalw28 <michal.waszak@yahoo.com>
2018-10-22 16:20:54 +0200

    u

--- assets/gtag.js
+++ assets/gtag.js
@@ -1,8 +1,8 @@
 
-  window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-  ga('create', 'UA-127839517-1', 'auto');
-  ga('send', 'pageview');
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-127839517-1', 'auto');
+ga('send', 'pageview');
 
-  async src='https://www.google-analytics.com/analytics.js'
+src='https://www.google-analytics.com/analytics.js'
 
  
